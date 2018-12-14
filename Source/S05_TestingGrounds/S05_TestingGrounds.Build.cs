a4e8b8541ce9151d5bfd6af050143f5d6a51e735
Craig Ferguson <craig@tworing.ca>
2018-12-13 17:37:57 -0800

    Start on AI Task implementation in C++

--- Source/S05_TestingGrounds/S05_TestingGrounds.Build.cs
+++ Source/S05_TestingGrounds/S05_TestingGrounds.Build.cs
@@ -8,7 +8,7 @@ public class S05_TestingGrounds : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "HeadMountedDisplay" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
