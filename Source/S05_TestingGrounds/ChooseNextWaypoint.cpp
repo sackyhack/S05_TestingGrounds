81d30cc9b03737c33778492436bb382c82f15276
Craig Ferguson <craig@tworing.ca>
2018-12-14 16:35:05 -0800

    Move ChooseNextWaypoint to C++

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,11 +2,18 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingCharacter.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	auto Owner = Cast<APatrollingCharacter>(OwnerComp.GetAIOwner()->GetPawn());
+
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, Owner->PatrolPointsCPP[Index]);
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, (Index + 1) % Owner->PatrolPointsCPP.Num());
 
 	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 
--- Source/S05_TestingGrounds/ChooseNextWaypoint.h
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.h
@@ -19,4 +19,8 @@ class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
+
 };
