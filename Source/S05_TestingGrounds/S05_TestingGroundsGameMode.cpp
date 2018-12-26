e0e0cf48abd5d722843296960df04f0082fc0c96
Craig Ferguson <craig@tworing.ca>
2018-12-25 16:21:55 -0800

    Have guard units hold gun actors

--- Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
+++ Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
@@ -8,7 +8,7 @@
 AS05_TestingGroundsGameMode::AS05_TestingGroundsGameMode()
 {
 	// Set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// Use our custom HUD class
